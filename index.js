67b26f4d2a4e41d3a5af94711de67d5ddaa5bd63
fenglong <fenglong@xiaomi.com>
2014-04-15 10:55:07 +0800

    更新方式

--- index.js
+++ index.js
@@ -2,18 +2,17 @@
 
 'use strict';
 
-var sass = require('fis-sass');
+var parserSass = require('fis-parser-sass');
 var root = fis.project.getProjectPath();
 var compass_root = __dirname + '/compass/frameworks';
 var frameworks_root = [compass_root + '/compass/stylesheets/', compass_root + '/blueprint/stylesheets/'];
 
 module.exports = function(content, file, conf){
-    var opts = fis.util.clone(conf);
-    opts.data = content;
-    
-    opts.include_paths = conf.include_paths || [];
-    opts.include_paths = opts.include_paths.concat([ file.dirname, root ]);
-    opts.include_paths = opts.include_paths.concat(frameworks_root);
 
-    return sass.renderSync(opts);
+    var include_paths = conf.include_paths || [root];
+    include_paths = include_paths.concat([ file.dirname, root ]);
+    include_paths = include_paths.concat(frameworks_root);
+    conf.include_paths = include_paths;
+    
+    return parserSass(content, file, conf);
 };
